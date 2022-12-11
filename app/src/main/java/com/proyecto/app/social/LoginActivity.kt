9cbd950f8546b5985f35f9372973a89e50132fa4
Carlos Cruz <cc197cc@gmail.com>
2022-12-11 01:06:50 -0600

    INICIO DE SESION

--- app/src/main/java/com/proyecto/app/social/LoginActivity.kt
+++ app/src/main/java/com/proyecto/app/social/LoginActivity.kt
@@ -1,12 +1,24 @@
 package com.proyecto.app.social
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.JsonObject
+import org.json.JSONObject
+
 
 class LoginActivity : AppCompatActivity() {
+
+    var loginUrl = "http://10.0.2.2:8080/app-api-identidad/api/identidad/sesion/iniciar"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -14,6 +26,8 @@ class LoginActivity : AppCompatActivity() {
         val recoverPassword  = findViewById(R.id.textView) as TextView
         val loginBtn = findViewById(R.id.btnCodigo) as Button
         val register = findViewById(R.id.textView2) as TextView
+        val userTxt = findViewById(R.id.editEmail) as TextView
+        val passwordTxt = findViewById(R.id.password) as TextView
 
         recoverPassword.setOnClickListener{
             val changePage = Intent(this, RecoverPasswordActivity::class.java)
@@ -21,8 +35,7 @@ class LoginActivity : AppCompatActivity() {
         }
 
         loginBtn.setOnClickListener{
-            val changePage = Intent(this, HomeActivity::class.java)
-            startActivity(changePage)
+            login(userTxt.text.toString(), passwordTxt.text.toString())
         }
 
         register.setOnClickListener{
@@ -31,4 +44,46 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
+    fun login(username:String ,password: String){
+        val queue = Volley.newRequestQueue(this@LoginActivity)
+
+        val json = JSONObject()
+        json.put("usuario",username)
+        json.put("contrasenia",password)
+        val requestBody = json.toString()
+        val stringReq : StringRequest =
+            object : StringRequest(Method.POST, loginUrl,
+                Response.Listener { response ->
+                    val json: JSONObject = JSONObject(response)
+                    val entidad = json["entidad"].toString()
+                    Log.e("Entidad", entidad)
+                    saveToken(entidad)
+                    Toast.makeText(this,"Inicio de sesion correcto",Toast.LENGTH_LONG).show()
+                    val changePage = Intent(this, HomeActivity::class.java)
+                    startActivity(changePage)
+                },
+                Response.ErrorListener { error ->
+                    val json = JSONObject(String(error.networkResponse.data))
+                    val mensaje = json["mensaje"].toString()
+                    Toast.makeText(this,"Error: " + mensaje,Toast.LENGTH_SHORT).show()
+                    Log.e("API2", "error => $error")
+                }
+            ) {
+                override fun getBodyContentType(): String {
+                    return "application/json;";
+                }
+
+                override fun getBody(): ByteArray {
+                    return requestBody.toByteArray()
+                }
+            }
+        queue.add(stringReq)
+    }
+
+    fun saveToken(token: String){
+        val sharedPreference =  getSharedPreferences("CREDENCIALES",Context.MODE_PRIVATE)
+        var editor = sharedPreference.edit()
+        editor.putString("token",token)
+        editor.commit()
+    }
 }
--- app/src/main/java/com/proyecto/app/social/ProfileActivity.kt
+++ app/src/main/java/com/proyecto/app/social/ProfileActivity.kt
@@ -1,7 +1,9 @@
 package com.proyecto.app.social
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ListView
 
 class ProfileActivity : AppCompatActivity() {
@@ -9,6 +11,7 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
+        val btnCloseSesion = findViewById(R.id.btnCodigo5) as Button
 
         var arraylist = ArrayList<Publicacion>();
         arraylist.add(Publicacion("Jose Manuel Velazques","Hace 5 horas","94285","Bache","Más de una semana sin agua en las unidad CTM Culhuacan Zona VI Coyoacan, Calle Manuela Cañizares  y mas texto de pruebas...","",true))
@@ -17,5 +20,18 @@ class ProfileActivity : AppCompatActivity() {
         mListView.adapter = publicacionAdapter
 
 
+        btnCloseSesion.setOnClickListener{
+            cerrarSesion(getToken())
+        }
+    }
+
+    private fun cerrarSesion(token: String) {
+
+    }
+
+    private fun getToken(): String {
+        val sharedPreference =  getSharedPreferences("CREDENCIALES", Context.MODE_PRIVATE)
+        var token: String = sharedPreference.getString("token","error").toString()
+        return token
     }
 }
